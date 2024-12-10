711c6855e7dab88ada755dd1582111396e950f5d
Jonas Wahle <wahle.jonas@web.de>
2024-12-10 11:54:35 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,60 @@
+<!-- Start Info & Footer Section -->
+<section class="info_section">
+    <div class="container">
+      <!-- Info Contact -->
+      <div class="contact_nav text-center">
+        <a href="" class="d-block">
+          <i class="fa fa-phone" aria-hidden="true"></i>
+          <span>Telefon: 123455678990</span>
+        </a>
+        <a href="" class="d-block">
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+          <span>Email: kontakt@bea-mohr.de</span>
+        </a>
+      </div>
+
+      <!-- Info Bottom -->
+      <div class="info_top">
+        <div class="row justify-content-center">
+          <!-- Info Navigation -->
+          <div class="row justify-content-center text-center mx-auto">
+            <div class="info_links">
+              <h4>Navigation</h4>
+              <div class="info_links_menu text-left">
+                <a href="index.php">Start</a>
+                <a href="ueber-mich.php">Über mich</a>
+                <a href="psychologische-beratung.php">Psychologische Beratung</a>
+                <a href="beratung-zur-persoenlichkeitsentwicklung.php">Beratung zur Persönlichkeitsentwicklung</a>
+                <a href="angebot.php">Angebot</a>
+                <a href="kontakt.php">Kontakt</a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Info Instagram -->
+          <div class="col-sm-6 col-md-4 col-lg-3 text-center my-2 m-auto">
+            <div class="info_post">
+              <h4>Instagram</h4>
+              <div id="box-shadow" class="embed-responsive embed-responsive-1by1 rounded-lg">
+                <iframe
+                  class="embed-responsive-item"
+                  src="https://www.instagram.com/bemore_80/embed/"
+                  allowfullscreen
+                  frameborder="0"
+                  scrolling="no">
+                </iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer Section -->
+  <footer class="footer_section text-center">
+    <div class="container">
+      <p>&copy; Bea Mohr 2024</p>
+    </div>
+  </footer>
+  <!-- End of Info & Footer Section -->
