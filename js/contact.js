be6a18619668dcbe200cd2a065adcc5c16199607
Jonas Wahle <wahle.jonas@web.de>
2024-12-10 22:26:56 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -16,3 +16,27 @@ emailInput.addEventListener("input", toggleRequiredAttributes);
 
 // Run the toggle function on page load to handle default states
 toggleRequiredAttributes();
+
+// Logic for preventing refresh on submit
+var form = document.getElementById("contact-form");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+function showSuccessMessage() {
+    let timerInterval;
+    Swal.fire({
+        customClass: {
+            confirmButton: 'confirm-button-class',
+        },
+        icon: "success",
+        title: "Anfrage erfolgreich versendet!",
+        timer: 2000,
+        timerProgressBar: true,
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+    }).then((result) => {
+        window.location.reload();
+    });
+
+}
