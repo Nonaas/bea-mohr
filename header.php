64abcadcca4dd4e5457e6b90050403ba25661a1d
Jonas Wahle <wahle.jonas@web.de>
2024-12-10 11:54:37 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,42 @@
+<!-- Start Header Section -->
+<header class="header_section px-0 fixed-top">
+    <nav class="navbar navbar-expand-lg custom_nav-container">
+      <a class="navbar-brand" href="index.php">
+        <img src="images/background.png" alt="Logo" 
+          class="img-fluid w-100" 
+          style="max-height: 100px; max-width: 100px; object-fit: scale-down;">
+        <span style="font-family: LTHumor; word-spacing: 10px;font-size: 1.8rem; ">
+          Beatrice Mohr
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class=""> </span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Startseite</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="ueber-mich.php">Über mich</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="psychologische-beratung.php">Psychologische Beratung</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="beratung-zur-persoenlichkeitsentwicklung.php">Beratung zur Persönlichkeitsentwicklung</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="angebot.php">Angebot</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kontakt.php">Kontakt</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- End Header Section -->
